b725826fed6c564623c5c6faef8c577ee9ababd7
alejandbel <bogdanov28c@gmail.com>
2022-11-11 10:12:49 +0300

    Added logging middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { logRequest } from '@middleware';
+
 const app = express();
 
 app.use(express.json());
+
+app.use(logRequest);
 
 async function init(): Promise<void> {
   try {
