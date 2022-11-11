f8879d2434708b80dca6b71cb0e0d60cf228b7d8
alejandbel <bogdanov28c@gmail.com>
2022-11-11 10:04:11 +0300

    Initialized express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Flashcards');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+async function init(): Promise<void> {
+  try {
+    app.listen(8080, () => console.log('Listening 8080'));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+init();
