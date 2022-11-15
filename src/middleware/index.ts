d3cf57319668edc73488d2569398d44ee384863a
alejandbel <bogdanov28c@gmail.com>
2022-11-16 01:54:52 +0300

    Refactored validate-payload.middleware.ts

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,4 @@
 export * from './log-request.middleware';
 export * from './process-error.middleware';
 export * from './not-found.middleware';
-export * from './validate-payload';
+export * from './validate-payload.middleware';
--- src/middleware/validate-payload.middleware.ts
+++ src/middleware/validate-payload.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 
-export const validatePayload = (req: Request, res: Response, next: NextFunction): void => {
+export const validatePayloadMiddleware = (req: Request, res: Response, next: NextFunction): void => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ message: 'Bad request', errors: errors.array() });
