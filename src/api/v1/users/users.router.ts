19d16cd12a56c43169c15a1b7f4b629ab6f4d011
alejandbel <bogdanov28c@gmail.com>
2022-11-16 17:29:48 +0300

    Added users validation

--- src/api/v1/users/users.router.ts
+++ src/api/v1/users/users.router.ts
@@ -2,10 +2,12 @@ import * as express from 'express';
 import { IRouter } from 'express';
 
 import { UsersController } from './users.controller';
+import { UsersValidation } from './users.validation';
+import { validatePayload } from '@middleware';
 
 const router = express.Router();
 
-router.post('/native-language', UsersController.changeLanguage);
+router.post('/native-language', UsersValidation.changeLanguage, validatePayload, UsersController.changeLanguage);
 
 export function mountRouter(routing: IRouter): void {
   routing.use('/users', router);
---
+++ src/api/v1/users/users.validation.ts
@@ -0,0 +1,19 @@
+import { checkSchema } from 'express-validator';
+
+export class UsersValidation {
+  static changeLanguage = checkSchema({
+    languageId: {
+      in: ['body'],
+      exists: {
+        errorMessage: 'Id is required',
+        options: { checkFalsy: true },
+      },
+      isInt: {
+        errorMessage: 'Invalid id',
+        options: {
+          min: 0,
+        },
+      },
+    },
+  });
+}
