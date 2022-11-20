d0c827b90fc0d279fccc2a106d4a1ac9c2dd91ce
alejandbel <bogdanov28c@gmail.com>
2022-11-21 00:50:47 +0300

    Annotated types in auth controller

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { AuthService } from './auth.service';
+import { TypedRequestBody } from '@interfaces';
 
 export class AuthController {
-  static signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  static signUp = async (
+    req: TypedRequestBody<{ email: string; password: string; name: string }>,
+    res: Response<{ token: string }>,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const { email, password, name } = req.body;
       const token = await AuthService.signUp(email, password, name);
@@ -12,7 +17,11 @@ export class AuthController {
     }
   };
 
-  static login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  static login = async (
+    req: TypedRequestBody<{ email: string; password: string }>,
+    res: Response<{ token: string }>,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const { email, password } = req.body;
       const token = await AuthService.login(email, password);
@@ -22,7 +31,7 @@ export class AuthController {
     }
   };
 
-  static logout = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  static logout = async (req: Request, res: Response<{ message: string }>, next: NextFunction): Promise<void> => {
     try {
       await AuthService.logout();
       const message = 'Successful logout';
