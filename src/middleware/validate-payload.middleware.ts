e74945674463f852d4b3ea051fbf0d3ac9d1806a
alejandbel <bogdanov28c@gmail.com>
2022-11-21 01:28:13 +0300

    Fixed validatePayload type problem

--- src/middleware/validate-payload.middleware.ts
+++ src/middleware/validate-payload.middleware.ts
@@ -2,7 +2,15 @@ import { NextFunction, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { KeyValueRequest } from '@interfaces';
 
-export const validatePayload = (req: KeyValueRequest, res: Response, next: NextFunction): void => {
+export const validatePayload = <
+  T1 extends Record<string, any>,
+  T2 extends Record<string, any>,
+  T3 extends Record<string, any>
+>(
+  req: KeyValueRequest<T1, T2, T3>,
+  res: Response,
+  next: NextFunction
+): void => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400).json({ message: 'Bad request', errors: errors.array() });
