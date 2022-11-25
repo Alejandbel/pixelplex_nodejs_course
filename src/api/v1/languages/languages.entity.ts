49aba7c51f09eb4c9d43a64c19c51907113c49ab
alejandbel <bogdanov28c@gmail.com>
2022-11-25 19:59:39 +0300

    Fixed typo

--- src/api/v1/languages/languages.entity.ts
+++ src/api/v1/languages/languages.entity.ts
@@ -1,11 +1,11 @@
-let laguagesCount = 0;
+let languagesCount = 0;
 
 export class Language {
   id: number;
 
   constructor(public readonly title: string, public readonly code: string) {
-    laguagesCount += 1;
-    this.id = laguagesCount;
+    languagesCount += 1;
+    this.id = languagesCount;
   }
 }
 
