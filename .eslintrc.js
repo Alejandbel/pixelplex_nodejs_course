9439a71b4b16c8ea9247113a5e24b963fa8ebfa3
alejandbel <bogdanov28c@gmail.com>
2022-11-25 21:51:26 +0300

    Updated eslint configuration types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,25 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      'alias': {
+        'map': [
+          ["@middleware", "./src/middleware"],
+          ["@errors", "./src/errors"],
+          ["@interfaces", "./src/interfaces"],
+          ["@constants", "./src/constants"],
+          ["@types", "./src/types"]
+        ],
+        'extensions': ['.ts', '.tsx'],
+      },
+    },
+  },
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -16,6 +30,30 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "import/order": [
+      "error",
+      {
+        "newlines-between": 'always',
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true
+        },
+        "groups": [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index"
+        ],
+        "pathGroups": [
+          {
+            "pattern": "@**",
+            "group": "external",
+            "position": "after",
+          }
+        ],
+      },
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'error',
