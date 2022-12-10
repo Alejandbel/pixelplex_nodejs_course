e36de155361d730c612f497e5b8dce041b84093c
alejandbel <bogdanov28c@gmail.com>
2022-12-10 14:19:56 +0300

    Created cards repository

---
+++ src/api/v1/cards/cards.repository.ts
@@ -0,0 +1,57 @@
+import { SORT_TYPES } from '@constants';
+import { AppError } from '@errors';
+import { Language } from '@languages';
+import { Word } from '@words';
+
+import { CARDS_ORDER_BY } from './cards.constants';
+import { Card } from './cards.entity';
+import { FindOptionsBuilder } from './cards.repository.utils';
+
+export class CardsRepository {
+  static create = async (nativeWord: Word, foreignWord: Language, userId: number): Promise<Card> => {
+    const card = Card.create({ nativeWord, foreignWord, userId });
+    return Card.save(card);
+  };
+
+  static update = async (id: number, props: Partial<Card>): Promise<Card> => {
+    const currentCard = await this.findById(id);
+    await Card.update({ id }, { ...currentCard, ...props });
+    return this.findById(id);
+  };
+
+  static findById = async (id: number): Promise<Card> => {
+    const card = await Card.findOneBy({ id });
+
+    if (!card) {
+      throw new AppError('Card does not exists', 404);
+    }
+
+    return card;
+  };
+
+  static delete = async (id: number): Promise<void> => {
+    await this.findById(id);
+    await Card.delete({ id });
+  };
+
+  static getAllSortedAndFilteredByUser = async (
+    limit: number,
+    offset: number,
+    orderBy: CARDS_ORDER_BY | undefined,
+    sort: SORT_TYPES | undefined,
+    search: string | undefined,
+    userId: number,
+    language: Language
+  ): Promise<Card[]> => {
+    const findOptions = new FindOptionsBuilder()
+      .applyLimitAndOffset(limit, offset)
+      .applySort(sort)
+      .applyOrderBy(orderBy)
+      .applySearch(language, search)
+      .applyUserId(userId)
+      .applyRelations()
+      .getFindOptions();
+
+    return Card.find(findOptions);
+  };
+}
