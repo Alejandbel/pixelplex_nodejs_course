19583ecadc0a2636645b68d8e2334d86e37c2c90
alejandbel <bogdanov28c@gmail.com>
2022-12-10 16:24:58 +0300

    Added data source initialization

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { APPLICATION_CONFIG } from '@config';
 import { logRequest, parseJson, processError, processNotFoundEndpoint } from '@middleware';
 
 import { mountRouter as mountApiRouter } from './api';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -18,6 +19,13 @@ app.use(processError);
 
 async function init(): Promise<void> {
   try {
+    await AppDataSource.initialize()
+      .then(() => {
+        console.log('Data Source has been initialized!');
+      })
+      .catch((err) => {
+        console.error('Error during Data Source initialization', err);
+      });
     app.listen(APPLICATION_CONFIG.PORT, () => console.log(`Listening on ${APPLICATION_CONFIG.PORT}`));
   } catch (error) {
     console.log(error);
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,13 @@
 import { DataSource } from 'typeorm';
 
+import { Card } from '@cards';
 import { DATASOURCE_CONFIG } from '@config';
 import { Language } from '@languages';
+import { Task } from '@tasks';
 import { User } from '@users';
+import { Word } from '@words';
+
+import { Answer } from './api/v1/answers/answers.entity';
 
 export const AppDataSource = new DataSource({
   type: DATASOURCE_CONFIG.TYPE,
@@ -13,5 +18,5 @@ export const AppDataSource = new DataSource({
   database: DATASOURCE_CONFIG.DATABASE,
   synchronize: DATASOURCE_CONFIG.SYNCHRONIZE,
   logging: DATASOURCE_CONFIG.LOGGING,
-  entities: [User, Language],
+  entities: [User, Language, Task, Card, Answer, Word],
 });
