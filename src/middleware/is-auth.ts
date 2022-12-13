724871ab24e410a1d9eddd03f29e83f818f76aba
alejandbel <bogdanov28c@gmail.com>
2022-12-13 19:34:38 +0300

    Made isAuth generic

--- src/middleware/is-auth.ts
+++ src/middleware/is-auth.ts
@@ -1,16 +1,20 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 
 import { JWT_SECRET } from '@config';
 import { AppError } from '@errors';
-import { TypedJwt } from '@interfaces';
+import { KeyValueRequest, TypedJwt } from '@interfaces';
 
-export function isAuth(req: Request, res: Response, next: NextFunction): void {
+export const isAuth = <T1 extends Record<string, any>, T2 extends Record<string, any>, T3 extends Record<string, any>>(
+  req: KeyValueRequest<T1, T2, T3>,
+  res: Response,
+  next: NextFunction
+): void => {
   try {
     const authHeader = req.get('Authorization');
 
     if (!authHeader) {
-      throw new AppError('Authorization header is missing', 400);
+      return next(new AppError('Authorization header is missing', 400));
     }
 
     const [, token] = authHeader.split(' '); // 'Bearer {{token}}'
@@ -29,4 +33,4 @@ export function isAuth(req: Request, res: Response, next: NextFunction): void {
     }
     next(error);
   }
-}
+};
