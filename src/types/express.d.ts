d56e23e73ebc3e251e0c646109b41f1287889376
alejandbel <bogdanov28c@gmail.com>
2022-12-15 23:36:53 +0300

    Fixed Request type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,6 @@ import { USER_ROLES } from '@users';
 declare global {
   declare namespace Express {
     export interface Request {
-      userId?: number;
       user?: {
         id: number;
         role: USER_ROLES;
