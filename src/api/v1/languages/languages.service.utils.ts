ad4e0cb778490fca1f97bc19e5e44d17d0d9ccc5
Aliaksandr Bahdanau <90352952+Alejandbel@users.noreply.github.com>
2022-12-21 15:56:16 +0300

    Updated languages service utils
    Co-authored-by: Tatsiana Palikarpava <111374305+t-polikarpova@users.noreply.github.com>

--- src/api/v1/languages/languages.service.utils.ts
+++ src/api/v1/languages/languages.service.utils.ts
@@ -1,11 +1,5 @@
 import { LanguagesRepository } from './languages.repository';
 
 export const isCodeUnique = async (code: string): Promise<boolean> => {
-  let isUnique = false;
-
-  if (!(await LanguagesRepository.findByCode(code))) {
-    isUnique = true;
-  }
-
-  return isUnique;
+  return !(await LanguagesRepository.findByCode(code));
 };
