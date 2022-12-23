94ab943ad8f1119bd3ca62648ec7d2277a6ea5f3
Aliaksandr Bahdanau <90352952+Alejandbel@users.noreply.github.com>
2022-12-23 18:01:24 +0300

    Update word utils ts
    Co-authored-by: Tatsiana Palikarpava <111374305+t-polikarpova@users.noreply.github.com>

--- src/api/v1/words/word.utils.ts
+++ src/api/v1/words/word.utils.ts
@@ -2,5 +2,5 @@ import { WordsRepository } from './words.repository';
 import { IWord } from './words.types';
 
 export const areWordsUnique = async (words: IWord[]): Promise<boolean> => {
-  return (await WordsRepository.findByWords(words)).length === 0;
+  return !(await WordsRepository.findByWords(words)).length;
 };
