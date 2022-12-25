3468c75948a2adc8ec4d945ee8559151baaa631e
alejandbel <bogdanov28c@gmail.com>
2022-12-25 17:33:09 +0300

    Divided express server and app

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import * as http from 'http';
+
+import { APPLICATION_CONFIG } from '@config';
+
+import { app } from './app';
+import { AppDataSource } from './data-source';
+
+const server = http.createServer(app);
+
+async function init(): Promise<void> {
+  try {
+    await AppDataSource.initialize()
+      .then(() => {
+        console.log('Data Source has been initialized!');
+      })
+      .catch((err) => {
+        console.error('Error during Data Source initialization', err);
+      });
+    server.listen(APPLICATION_CONFIG.PORT, () => console.log(`Listening on ${APPLICATION_CONFIG.PORT}`));
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+init();
